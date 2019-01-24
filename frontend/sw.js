4c8de7ef3e6a6f346f44a2b2466c3ed9ec1a53cb
airwater4 <airwater4@gmail.com>
2019-01-25 00:22:37 +0100

    updated service worker allow only https connections

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'afvalkalender-cache-v1';
+var CACHE_NAME = 'afvalkalender-cache-v2';
 var urlsToCache = [
   '/',
   '/index.html'
@@ -19,39 +19,33 @@ self.addEventListener('fetch', event => {
         if (response) return response;
 
         return fetch(event.request).then(response => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches
-              .open(CACHE_NAME)
-              .then(cache => cache.put(event.request, responseToCache));
-
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
             return response;
           }
+
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
+          var responseToCache = response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then(cache => cache.put(event.request, responseToCache));
+
+          return response;
+        }
         );
       })
-    );
+  );
 });
 
 self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
   event.waitUntil(
-    caches.keys().then(cacheNames => 
+    caches.keys().then(cacheNames =>
       Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.map(cacheName => caches.delete(cacheName))
       )
     )
   );
