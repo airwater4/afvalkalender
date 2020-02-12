2dde580efde0f60e526ca443950a978e8932fe64
Dmitriy Ievlev <ievlevdmitriy@gmail.com>
2020-02-13 00:01:58 +0100

    ddded test to check that SW caches resources

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,16 +1,41 @@
 const isDev = process.env.NODE_ENV === 'development'
 const PORT = process.env.PORT || 3000
+const LOCAL_URL = `http://localhost:${PORT}/`
+const PUBLIC_URL = 'https://afvalkalender.appspot.com/'
+const PAGE_URL = isDev ? LOCAL_URL : PUBLIC_URL
+const SW_TARGET = 'service_worker'
 
 describe('afvalkalender', () => {
   beforeAll(async () => {
-    if (isDev) {
-      await page.goto(`http://localhost:${PORT}/`)
-    } else {
-      await page.goto('https://afvalkalender.appspot.com/')
-    }
+    await page.goto(PAGE_URL)
   })
 
-  it('should display "google" text on page', async () => {
+  it('should display "afvalkalender" text on page', async () => {
     await expect(page).toMatch('afvalkalender')
+  })
+})
+
+describe('service worker', () => {
+  it('should register a service worker', async () => {
+    const newPage = await context.newPage()
+    await newPage.goto(PAGE_URL)
+    const swTarget = await context.waitForTarget(target => {
+      return target.type() === SW_TARGET
+    })
+  })
+
+  it('should cache resources', async () => {
+    const newPage = await context.newPage()
+    await newPage.goto(PAGE_URL)
+
+    const swTarget = await context.waitForTarget(target => {
+      return target.type() === SW_TARGET
+    })
+    const serviceWorker = await swTarget.worker()
+    const isCached = await serviceWorker.evaluate(async () => {
+      const PAGE_URL = 'http://localhost:3000' // TODO get rid off harcoded value
+      return !!(await caches.match(`${PAGE_URL}/js/main.js`))
+    })
+    expect(isCached).toBe(true)
   })
 })
